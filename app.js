6d328da8e7cf5c04b74824566597c872a7cce865
Nurullo <khuwaidullaev@gmail.com>
2021-02-15 19:17:27 +0500

    Adding Basic Styles/Bootstrap 5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const engine = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -18,12 +19,13 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', engine);
+
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-
 
 //Order matters!!!
 app.get("/", (req, res) => {
@@ -64,7 +66,12 @@ app.put("/campgrounds/:id", async (req, res) => {
 app.delete("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds")
+    res.redirect("/campgrounds");
+});
+
+//Place at the end!!!
+app.use((req, res) => {
+    res.status(404).send("NOT FOUND")
 });
 
 app.listen(3000, () => {
