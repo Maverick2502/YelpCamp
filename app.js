c64639a75da95ddc9895e82cac22a63f1415ccc2
Nurullo <khuwaidullaev@gmail.com>
2021-02-18 15:34:13 +0500

    Configuring Session

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const engine = require("ejs-mate");
-const { campgroundSchema, reviewSchema } = require("./schemas")
-const catchAsync = require("./utils/catchAsync");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
+
 const campgrounds = require("./routes/campgrounds")
 const reviews = require("./routes/reviews")
 
@@ -35,6 +33,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+    secret: "maverick2502",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:  1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 //Setting our routes!!!
 app.use("/campgrounds", campgrounds)
