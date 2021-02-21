f5cf964b5efdbbf19a4c4889b5608e04a7edcbea
Nurullo <khuwaidullaev@gmail.com>
2021-02-21 12:40:17 +0500

    Authorization Middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
  //expressRouter keeps params separately, 
  //so as not to let it happen we should merge params
 const router = express.Router({ mergeParams: true });
@@ -7,18 +6,9 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const { campgroundSchema, reviewSchema } = require("../schemas")
+const { validateReview } = require("../middleware");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-
-const validateReview = (req, res, next) => {
-    const { error }  = reviewSchema.validate(req.body);
-      if (error) {
-        const msg = error.details.map(el => el.message).join(".")
-        throw new ExpressError(msg, 400)
-    }else {
-        next();
-    }
-}
 
 router.post("/", validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
