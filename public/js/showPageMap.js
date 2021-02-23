48cb2049753f431dcf6da5fd661c8365504e2ea9
Nurullo <khuwaidullaev@gmail.com>
2021-02-23 10:28:59 +0500

    Centering the Map on a Campground

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,11 +2,10 @@
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
-        center: [70.8503354718028, 38.63103862957675],
+        center: campground.geometry.coordinates,
         zoom: 4
     });
 
-
 new mapboxgl.Marker()
-    .setLngLat([70.8503354718028, 38.63103862957675])
+    .setLngLat(campground.geometry.coordinates)
     .addTo(map);
