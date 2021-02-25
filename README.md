4a13f4d104266c34271f1abb6003b7fe1acfe005
Nurullo <khuwaidullaev@gmail.com>
2021-02-25 09:34:13 +0500

    Added README.md

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # YelpCamp
-### YelpCamp is one of the projects done as part of the Web Development bootcamp.
+> YelpCamp is one of the projects done as part of the  course - [The Web Developer Bootcamp by Colt Steele](https://www.udemy.com/the-web-developer-bootcamp/)
 ### It is a web application designed to add, rate and review different campgrounds, different users (read campers) can put in their comments and concerns, so that it is a well informed and well prepared camping trip for other users.
-
-### This is UNFINISHED PROJECT yet. Changes are being added on a daily basis.
 
  The code needs the following environment variables:
  - CLOUDINARY_CLOUD_NAME
  - CLOUDINARY_KEY
  - CLOUDINARY_SECRET
+ - MAPBOX_TOKEN* (included in the code)
  
  All of these are obtainable from your Cloudinary account.
  
@@ -16,5 +15,44 @@
 
  Then, run `nodemon` or `node app.js` to start the server.
 
+ In case, you need to update your seeds run `node seeds/index.js`
+
  Access through http://localhost:3000/
+
+ The app has been deployed with Heroku, and can be accessed through https://yelp-camp-2502.herokuapp.com/
+
+## Built with
+
+### Front-end
+
+* [ejs](http://ejs.co/)
+* [Bootstrap](https://getbootstrap.com/docs/3.3/)
+
+### Back-end
+
+* [express](https://expressjs.com/)
+* [mongoDB](https://www.mongodb.com/)
+* [mongoose](http://mongoosejs.com/)
+* [async](http://caolan.github.io/async/)
+* [passport](http://www.passportjs.org/)
+* [passport-local](https://github.com/jaredhanson/passport-local#passport-local)
+* [express-session](https://github.com/expressjs/session#express-session)
+* [method-override](https://github.com/expressjs/method-override#method-override)
+* [connect-flash](https://github.com/jaredhanson/connect-flash#connect-flash)
+
  
+
+### Feel free to clone and play around
+
+```sh
+git clone https://github.com/Maverick2502/YelpCamp.git
+          
+```
+ # Screenshots
+
+![](Screenshots/Home-Page.jpg)
+![](Screenshots/Index-Page-1.jpg)
+![](Screenshots/Index-Page-2.jpg)
+![](Screenshots/Login-Page.jpg)
+![](Screenshots/New-Page.jpg)
+![](Screenshots/View-Page.jpg)
--- app.js
+++ app.js
@@ -1,6 +1,134 @@
+// // Deployment ready version of the CODE BELOW!!!!
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config();
+// }
+
+// const express = require("express");
+// const path = require("path");
+// const mongoose = require("mongoose");
+// const engine = require("ejs-mate");
+// const session = require("express-session");
+// const flash = require('connect-flash');
+// const ExpressError = require("./utils/ExpressError");
+// const methodOverride = require("method-override");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local");
+// const User = require("./models/user");
+
+// const mongoSanitize = require('express-mongo-sanitize');
+
+// const userRoutes = require("./routes/users")
+// const campgroundRoutes = require("./routes/campgrounds");
+// const reviewRoutes = require("./routes/reviews");
+
+// const MongoStore = require('connect-mongo').default;
+
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+// mongoose.connect(dbUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// });
+
+// const db = mongoose.connection; // for brevity
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
+
+// const app = express();
+
+// // use ejs-locals for all ejs templates:
+// app.engine('ejs', engine);
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(methodOverride("_method"));
+// app.use(express.static(path.join(__dirname, "public")));
+// // To remove data, use:
+// app.use(mongoSanitize());
+
+// const secret = process.env.SECRET || "keep coding";
+
+// const store = MongoStore.create({
+//     mongoUrl: dbUrl,
+//     secret,
+//     touchAfter: 24 * 60 * 60
+// });
+
+// const sessionConfig = {
+//     store,
+//     name: "_asht",
+//     secret,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         httpOnly: true,
+//         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+//         maxAge:  1000 * 60 * 60 * 24 * 7
+//     }
+// }
+// app.use(session(sessionConfig))
+// app.use(flash());
+
+// app.use(passport.initialize());
+// app.use(passport.session())
+// passport.use(new LocalStrategy(User.authenticate()));
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// app.use((req, res, next) => {
+//     res.locals.currentUser = req.user;
+//     res.locals.success = req.flash("success");
+//     res.locals.error = req.flash("error");
+//     next();
+// });
+
+// //Setting our routes!!!
+// app.use("/", userRoutes);
+// app.use("/campgrounds", campgroundRoutes)
+// app.use("/campgrounds/:id/reviews", reviewRoutes)
+
+// //Order matters!!!
+// app.get("/", (req, res) => {
+//     res.render("home")
+// });
+
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError("Page not found", 404))
+// })
+
+// //Place at the end!!!
+// app.use((req, res) => {
+//     res.status(404).render("campgrounds/404")
+// });
+
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500 } = err;
+//     if(!err.message) err.message = "Damn, Something Went Wrong Maan!"
+//     res.status(statusCode).render("campgrounds/404", { err });
+// });
+
+// //Defining 'port' for Heroku and for running on our 'localhost:3000'
+// const port = process.env.PORT || 3000;
+
+// app.listen(port, () => {
+//     console.log("Your server is up and running");
+// }); 
+
+
+
+
+//Localhost version with all saved seeds
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
+console.log(process.env.SECRET);
 
 const express = require("express");
 const path = require("path");
@@ -20,10 +148,11 @@ const userRoutes = require("./routes/users")
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-// const session = require('express-session');
 const MongoStore = require('connect-mongo').default;
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = process.env.DB_URL;
+
+const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -52,18 +181,16 @@ app.use(express.static(path.join(__dirname, "public")));
 // To remove data, use:
 app.use(mongoSanitize());
 
-const secret = process.env.SECRET || "keep coding";
-
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    secret,
+    secret: "keep coding",
     touchAfter: 24 * 60 * 60
 });
 
 const sessionConfig = {
     store,
     name: "_asht",
-    secret,
+    secret: "maverick2502",
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -114,8 +241,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("campgrounds/404", { err });
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log("Your server is up and running");
 }); 
